c477739310a8d40a010f2a9572e43655fe1cb6c6
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-12 07:25:36 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: {
+        dark: '#00C889',
+        regular: '#15E49A',
+        light: '#37FFBF'
+      },
+      gray: {
+        dark: '#202020',
+        regular: '#49454C',
+        light: '#868389'
+      },
+      white: {
+        dark: '#DADADA',
+        regular: '#EDEDED',
+        light: '#F9F9F9'
+      }
+    },
+    fontFamily: {
+      saira: ['Saira Condensed', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
