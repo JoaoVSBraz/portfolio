e525d95598f294cc956306e1ddc28f42ad22beda
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-13 09:21:28 -0300

    Revert "import image assets"
    This reverts commit db2265f5b1acdd7f20af089bbf2da4091b35c7f4.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import FirstScreen from "./components/FirstScreen/FirstScreen"
-import SecondScreen from "./components/SecondScreen/SecondScreen"
 
 export default function App() {
   return (
     <main className="bg-gray-dark font-saira font-medium text-center">
       <FirstScreen />
-      <SecondScreen />
     </main>
   )
 }
