991e633223efc4231ebf2585bd6f932b79ff5714
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-13 09:27:40 -0300

    import SecondScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import FirstScreen from "./components/FirstScreen/FirstScreen"
+import SecondScreen from "./components/SecondScreen/SecondScreen"
 
 export default function App() {
   return (
     <main className="bg-gray-dark font-saira font-medium text-center">
       <FirstScreen />
+      <SecondScreen />
     </main>
   )
 }
