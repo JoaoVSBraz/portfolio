2ab5472c0954c1b74e049549fd9a58318199ae6a
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-13 07:34:20 -0300

    firstScreen mobile layout complete

--- src/components/FirstScreen/FirstScreen.jsx
+++ src/components/FirstScreen/FirstScreen.jsx
@@ -1,7 +1,7 @@
 export default function FirstScreen(){
     return (
-        <div className="container h-screen">
-            <header className="bg-white-regular border-b-[1px] border-white-dark w-full">
+        <div className="container flex flex-col h-screen">
+            <header className="bg-white-regular border-b-[1px] border-white-dark w-full h-fit">
                 <nav className="p-5">
                     <ul className="flex justify-between">
                         <li><h1 className="text-2xl font-medium">João <span>Braz</span></h1></li>
@@ -13,22 +13,27 @@ export default function FirstScreen(){
                     </ul>
                 </nav>
             </header>
-            <section id="hero" className="container relative bg-white-regular text-gray-dark h-full">
-                <h2 className="text-5xl w-1/2 mx-auto pt-8 pb-4">Olá, sou João Braz</h2>
-                <h3 className="text-xl font-medium mb-4">Desenvolvedor Fullstack</h3>
-
-                <h4 className="text-primary-regular mb-4">Resumo</h4>
-
-                <p className="mb-2">Desenvolvedor fullstack localizado em Pernambuco, Recife.</p>
-                <p className="mb-8">Comecei meu caminho no desenvolvimento web por hobby. Através da tecnologia, dar vida a projetos que transformam e melhoram a forma como lidamos com o mundo, é sempre muito empolgante.</p>
-
-                <div className="flex justify-evenly">
-                    <button className="py-2 px-4 bg-primary-regular border-4">CONTATO</button>
-                    <button className="py-2 px-4 bg-primary-regular border-4">DOWNLOAD CV</button>
+            <section id="hero" className="container bg-white-regular text-gray-dark flex flex-col h-full">
+                <div>
+                    <h2 className="text-5xl w-1/2 mx-auto pt-8 pb-4">Olá, sou João Braz</h2>
+                    <h3 className="text-xl font-medium mb-4">Desenvolvedor Fullstack</h3>
                 </div>
 
+                <div>
+                    <h4 className="text-primary-regular mb-4">Resumo</h4>
+
+                    <p className="mb-2">Desenvolvedor fullstack localizado em Pernambuco, Recife.</p>
+                    <p className="mb-8">Comecei meu caminho no desenvolvimento web por hobby. Através da tecnologia, dar vida a projetos que transformam e melhoram a forma como lidamos com o mundo, é sempre muito empolgante.</p>
+
+                    <div className="flex justify-evenly">
+                        <button className="py-2 px-4 bg-primary-regular border-4">CONTATO</button>
+                        <button className="py-2 px-4 bg-primary-regular border-4">DOWNLOAD CV</button>
+                    </div>
+                </div>
                 
-                <hr className="w-8 h-8 absolute bottom-0.5 rotate-90 border-dashed border-t-2" />
+                <div className="relative flex h-full items-center justify-center">
+                    <div className="h-8 absolute bottom-0 border-dashed border-r-2"></div>
+                </div>
                 
             </section>
         </div>
