8cf0d0db2b3702dbd3adb42901d8a4e5e3ed9823
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-16 08:10:08 -0300

    complete second screen layout

--- src/components/SecondScreen/SecondScreen.jsx
+++ src/components/SecondScreen/SecondScreen.jsx
@@ -1,34 +1,65 @@
 import ArticleCard from "./ArticleCard"
 
-const cards = [
+const frontCards = [
     {
         imageSource: './joaovsbraz-portfolio.png',
         imageAlt: 'Meu Portfólio',
         cardTitle: 'Portfólio',
-        cardContent: 'Construído com React e Styled Components. Confira o layout completo no meu perfil Dribble.',
+        cardContent: 'Construído com React, Vite e TailwindCSS. Confira a construção do layout no meu perfil Dribble.',
         cardLink: '#'
     },
     {
         imageSource: './detran-pe.png',
         imageAlt: 'Site institucional do Departamento de Trânsito de Pernambuco',
         cardTitle: 'Detran PE',
-        cardContent: 'Atualmente faço parte do equipe de desenvolvimento web no Departamento de Trânsito de Pernambuco.',
+        cardContent: 'Em conjunto com a equipe web, desenvolvi o atual site do Departamento de Trânsito de Pernambuco.',
         cardLink: '#'
     },
+    {
+        imageSource: './ignite-lab.png',
+        imageAlt: 'Plataforma de vídeo-aulas online construída durante o evento Ignite Lab pela Rocketseat',
+        cardTitle: 'Ignite Lab',
+        cardContent: 'Plataforma de vídeo-aulas online construída com React, Apollo Client, GraphQL e GraphCMS. ',
+        cardLink: '#'
+    },
+]
 
+const backCards = [
+    {
+        imageSource: './roquet-q.png',
+        imageAlt: 'RoquetQ - Plataforma de perguntas e respostas',
+        cardTitle: 'Roquet Q',
+        cardContent: 'Plataforma de perguntas e respostas baseada em salas anônimas construída com JS, Node e Sqlite.',
+        cardLink: '#'
+    },
 ]
 
 export default function SecondScreen() {
     return (
         <div className="container flex flex-col h-screen">
-            <section id="projects">
-                <h2>PROJETOS</h2>
-                <div>
-                    <button>Frontend</button>
-                    <button>Backend</button>
+            <h2 className="text-white-light text-[32px]">PROJETOS</h2>
+            <section id="projects" className="flex flex-col h-full align-center justify-center">
+                <div className="flex align-center justify-evenly text-white-light
+text-white-light text-xl mb-8">
+                    <button className="py-2 px-4 text-primary-regular underline">Frontend</button>
+                    <button className="py-2 px-4">Backend</button>
                 </div>
-                <div>
-                    {cards.map((card) => {
+                <div className="flex gap-x-8 overflow-x-auto columns-4 py-4 px-8">
+                    {frontCards.map((card) => {
+                        return (
+                            <ArticleCard
+                            key={card.cardTitle}
+                            imageSource={card.imageSource}
+                            imageAlt={card.imageAlt}
+                            cardTitle={card.cardTitle}
+                            cardContent={card.cardContent}
+                            cardLink={card.cardLink}
+                        />
+                        )
+                    })}
+                </div>
+                <div className="flex gap-2 overflow-x-auto columns-4 py-4 px-8 hidden">
+                    {backCards.map((card) => {
                         return (
                             <ArticleCard
                             key={card.cardTitle}
@@ -42,6 +73,9 @@ export default function SecondScreen() {
                     })}
                 </div>
             </section>
+            <div className="relative flex h-min-fit items-center justify-center">
+                    <div className="h-8 absolute bottom-0 border-dashed border-r-2 border-white-light"></div>
+                </div>
         </div>
     )
 }
