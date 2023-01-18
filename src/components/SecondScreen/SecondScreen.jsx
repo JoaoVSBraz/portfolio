9e0577f66186b8aab4f5603ebc18320332440c1c
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-18 13:57:40 -0300

    add composing elements and toggle frontend/backend buttons

--- src/components/SecondScreen/SecondScreen.jsx
+++ src/components/SecondScreen/SecondScreen.jsx
@@ -34,17 +34,45 @@ const backCards = [
     },
 ]
 
+function handleButton(e){
+   const target = e.target
+   const frontButton = document.querySelector('#front')
+   const backButton = document.querySelector('#back')
+   const frontCards = document.querySelector('#front-cards')
+   const backCards = document.querySelector('#back-cards')
+
+   if(target.id === 'front'){
+    target.classList.add('active')
+    backButton.classList.remove('active')
+    frontCards.classList.remove('hidden')
+    backCards.classList.add('hidden')
+   }else{
+    target.classList.add('active')
+    frontButton.classList.remove('active')
+    backCards.classList.remove('hidden')
+    frontCards.classList.add('hidden')
+
+   }
+
+}
+
 export default function SecondScreen() {
     return (
-        <div id="projects" className="container flex flex-col h-screen">
-            <h2 className="text-white-light text-[32px] mt-[48px]">PROJETOS</h2>
-            <section className="flex flex-col h-full align-center justify-center">
-                <div className="flex align-center justify-evenly text-white-light
-text-white-light text-xl mb-8">
-                    <button className="py-2 px-4 text-primary-regular underline">Frontend</button>
-                    <button className="py-2 px-4">Backend</button>
+        <div id="projects" className="relative container flex flex-col h-screen overflow-hidden">
+            <section id="composing-elements z-0">
+                <div className="absolute blur opacity-25 left-0 top-10 w-40 h-40 rounded-full bg-primary-dark"></div>
+                <div className="absolute blur opacity-25 right-[-80px] top-40 w-52 h-52 rounded-full bg-primary-dark"></div>
+                <div className="absolute blur opacity-25 left-[-80px] bottom-[-64px] w-64 h-64 rounded-full bg-primary-dark"></div>
+                <div className="absolute blur opacity-25 right-8 bottom-8 w-14 h-14 rounded-full bg-primary-dark"></div>
+            </section>
+            <h2 className="text-white-light text-[32px] mt-[48px] z-10">PROJETOS</h2>
+            <section className="flex flex-col h-full align-center justify-center z-10">
+                <div id="mobile-buttons" className="flex align-center justify-evenly text-white-light
+text-white-light text-xl mb-8" onClick={handleButton}>
+                    <button id="front" type="button" className="py-2 px-4 active">Frontend</button>
+                    <button id="back" type="button" className="py-2 px-4">Backend</button>
                 </div>
-                <div className="flex gap-x-8 overflow-x-auto columns-4 py-4 px-8">
+                <div id="front-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8">
                     {frontCards.map((card) => {
                         return (
                             <ArticleCard
@@ -58,7 +86,7 @@ text-white-light text-xl mb-8">
                         )
                     })}
                 </div>
-                <div className="flex gap-2 overflow-x-auto columns-4 py-4 px-8 hidden">
+                <div id="back-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8 hidden">
                     {backCards.map((card) => {
                         return (
                             <ArticleCard
