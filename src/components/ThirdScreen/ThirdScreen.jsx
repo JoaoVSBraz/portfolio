eb3760916293ce9d40f607f55c09bb03e1193335
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-19 08:34:19 -0300

    update elements margin and padding on ThirdScreen mobile layout

--- src/components/ThirdScreen/ThirdScreen.jsx
+++ src/components/ThirdScreen/ThirdScreen.jsx
@@ -9,7 +9,7 @@ export default function ThirdScreen(){
     return (
         <div id='contact' className="container flex flex-col h-screen">
             <div className="bg-white-regular h-[50vh]">
-                <section id="skills" className="flex align-center justify-center gap-8 overflow-x-hidden my-8 p-6">
+                <section id="skills" className="flex align-center justify-center gap-8 overflow-x-hidden mt-20 mb-8 p-6">
                     <img src={htmlSvg} alt="html svg" />
                     <img src={cssSvg} alt="css svg" />
                     <img src={jsSvg} alt="js svg" />
@@ -17,10 +17,10 @@ export default function ThirdScreen(){
                     <img src={bootstrapSvg} alt="bootstrap svg" />
                     <img src={tailwindSvg} alt="tailwind svg" />
                 </section>
-                <p className='text-[32px] p-8 flex align-center m-auto'>Tem uma ideia de projeto legal?</p>
+                <p className='text-[32px] p-4 flex align-center m-auto'>Tem uma ideia de projeto legal?</p>
             </div>
             <footer className='flex flex-col h-full justify-between place-items-center bg-gray-dark'>
-                <p className='flex text-[32px] p-8 underline text-primary-regular'>joaovsbraz@gmail.com</p>
+                <p className='flex text-[32px] p-4 underline text-primary-regular'>joaovsbraz@gmail.com</p>
                 <p className='flex w-full justify-center text-sm text-white-light p-8'>2023 © Desenhado e desenvolvido por João Braz</p>
             </footer>
         </div>
