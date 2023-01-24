def2b82eba03fa9606c52f59bd86f6d5be14d755
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-24 10:35:11 -0300

    update SecondScreen to large screen devices

--- src/components/SecondScreen/ArticleCard.jsx
+++ src/components/SecondScreen/ArticleCard.jsx
@@ -1,13 +1,14 @@
 export default function ArticleCard(props){
     return (
-        <article className="flex flex-col drop-shadow-card h-[344px] min-w-[242px] max-w-[242px] bg-white-light">
+        <article className="flex flex-col drop-shadow-card min-h-[300px] h-[300px] min-w-[180px] max-w-[180px] bg-white-light
+        2xl:min-h-[340px] 2xl:min-w-[220px]">
             <img src={props.imageSource} alt={props.imageAlt} />
-            <div className="flex flex-col bg-white-light h-full items-center justify-between py-2 px-6">
+            <div className="flex flex-col bg-white-light h-full items-center justify-between py-2 mb-2 px-4">
                 <h3 className="">{props.cardTitle}</h3>
 
-                <p>{props.cardContent}</p>
+                <p className="text-base">{props.cardContent}</p>
 
-                <button type="button" className="w-full py-[4px] bg-primary-regular outline outline-4 text-2xl"><a href={props.cardLink}>ACESSAR</a></button>
+                <button type="button" className="w-full py-[2px] bg-primary-regular outline outline-4 text-2xl"><a href={props.cardLink}>ACESSAR</a></button>
             </div>
         </article>
     )
--- src/components/SecondScreen/SecondScreen.jsx
+++ src/components/SecondScreen/SecondScreen.jsx
@@ -1,4 +1,5 @@
 import ArticleCard from "./ArticleCard"
+import { useState } from "react"
 
 const frontCards = [
     {
@@ -29,83 +30,119 @@ const backCards = [
         imageSource: './roquet-q.png',
         imageAlt: 'RoquetQ - Plataforma de perguntas e respostas',
         cardTitle: 'Roquet Q',
-        cardContent: 'Plataforma de perguntas e respostas baseada em salas anônimas construída com JS, Node e Sqlite.',
+        cardContent: 'Plataforma de perguntas e respostas baseada em salas construída com JS, Node e Sqlite.',
+        cardLink: '#'
+    },{
+        imageSource: './roquet-q.png',
+        imageAlt: 'RoquetQ - Plataforma de perguntas e respostas',
+        cardTitle: 'Roquet Q',
+        cardContent: 'Plataforma de perguntas e respostas baseada em salas construída com JS, Node e Sqlite.',
+        cardLink: '#'
+    },{
+        imageSource: './roquet-q.png',
+        imageAlt: 'RoquetQ - Plataforma de perguntas e respostas',
+        cardTitle: 'Roquet Q',
+        cardContent: 'Plataforma de perguntas e respostas baseada em salas construída com JS, Node e Sqlite.',
         cardLink: '#'
     },
 ]
 
-function handleButton(e){
-   const target = e.target
-   const frontButton = document.querySelector('#front')
-   const backButton = document.querySelector('#back')
-   const frontCards = document.querySelector('#front-cards')
-   const backCards = document.querySelector('#back-cards')
-
-   if(target.id === 'front'){
-    target.classList.add('active')
-    backButton.classList.remove('active')
-    frontCards.classList.remove('hidden')
-    backCards.classList.add('hidden')
-   }else{
-    target.classList.add('active')
-    frontButton.classList.remove('active')
-    backCards.classList.remove('hidden')
-    frontCards.classList.add('hidden')
-
-   }
-
-}
-
 export default function SecondScreen() {
+
+    const isLargeScreen = window.innerWidth >= 1280
+
+    function handleButton(e) {
+        const target = e.target
+        const frontButton = document.querySelector('#front')
+        const backButton = document.querySelector('#back')
+        const frontCards = document.querySelector('#front-cards')
+        const backCards = document.querySelector('#back-cards')
+
+        if(window.innerWidth >= 1280){
+            return;
+        }
+        
+        if(target.id === 'front'){
+            target.classList.add('active')
+            backButton.classList.remove('active')
+            frontCards.classList.remove('hidden')
+            backCards.classList.add('hidden')
+        }else{
+            target.classList.add('active')
+            frontButton.classList.remove('active')
+            backCards.classList.remove('hidden')
+            frontCards.classList.add('hidden')
+
+        }
+    }
+
     return (
-        <div id="projects" className="relative container flex flex-col h-screen overflow-hidden">
+        <div id="projects" className="relative flex flex-col h-screen overflow-hidden md:p-16 md:justify-evenly xl:p-0 xl:justify-center">
             <section id="composing-elements z-0">
                 <div className="absolute blur opacity-25 left-0 top-10 w-40 h-40 rounded-full bg-primary-dark
-                md:w-60 md:h-60"></div>
+                md:w-60 md:h-60
+                xl:left-24 xl:-top-16"></div>
                 <div className="absolute blur opacity-25 right-[-80px] top-40 w-52 h-52 rounded-full bg-primary-dark
-                md:w-60 md:h-60 md:top-64"></div>
+                md:w-60 md:h-60 md:top-64
+                xl:top-10"></div>
                 <div className="absolute blur opacity-25 left-[-80px] bottom-[-64px] w-64 h-64 rounded-full bg-primary-dark
-                md:w-[572px] md:h-[572px] md:left-[-200px]"></div>
+                md:w-[572px] md:h-[572px] md:left-[-200px]
+                xl:-bottom-32"></div>
                 <div className="absolute blur opacity-25 right-8 bottom-8 w-14 h-14 rounded-full bg-primary-dark
-                md:bottom-32"></div>
+                md:bottom-32
+                xl:bottom-8"></div>
             </section>
             <h2 className="text-white-light text-[32px] mt-[64px] z-10
-            md:text-[64px] md:mt-28">PROJETOS</h2>
+            md:text-5xl xl:m-0 xl:text-[64px]">PROJETOS</h2>
+            <div id="mobile-buttons" className="flex align-center justify-evenly text-white-light
+text-white-light text-xl mt-8 z-10
+xl:justify-around xl:mt-16
+2xl:mt-24" onClick={handleButton}>
+
+                <button id="front" type="button" className={`py-2 px-4 md:text-2xl md:py-2 xl:text-[24px] ${isLargeScreen && 'active'}`}>Frontend</button>
+
+                <button id="back" type="button" className={`py-2 px-4 md:text-2xl md:py-2 xl:text-[24px] ${isLargeScreen && 'active'}`}>Backend</button>
+
+            </div>
             <section className="flex flex-col h-min my-auto z-10
-            md:w-5/6 md:mx-auto md:my-40">
-                <div id="mobile-buttons" className="flex align-center justify-evenly text-white-light
-text-white-light text-xl mb-4" onClick={handleButton}>
-                    <button id="front" type="button" className="py-2 px-4 md:text-[32px] md:py-8 active">Frontend</button>
-                    <button id="back" type="button" className="py-2 px-4 md:text-[32px] md:py-8">Backend</button>
-                </div>
-                <div id="front-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8">
-                    {frontCards.map((card) => {
-                        return (
-                            <ArticleCard
-                            key={card.cardTitle}
-                            imageSource={card.imageSource}
-                            imageAlt={card.imageAlt}
-                            cardTitle={card.cardTitle}
-                            cardContent={card.cardContent}
-                            cardLink={card.cardLink}
-                        />
-                        )
-                    })}
-                </div>
-                <div id="back-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8 hidden">
+             md:mx-auto md:my-8
+             xl:m-0 xl:flex-row xl:mx-auto xl:gap-8 xl:mt-16">
+                
+                    <div id="front-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8 active
+                    xl:px-2 xl:gap-x-4
+                    2xl:px-4 2xl:gap-x-8">
+                        {frontCards.map((card) => {
+                            return (
+                                <ArticleCard
+                                    key={card.cardTitle}
+                                    imageSource={card.imageSource}
+                                    imageAlt={card.imageAlt}
+                                    cardTitle={card.cardTitle}
+                                    cardContent={card.cardContent}
+                                    cardLink={card.cardLink}
+                                />
+                            )
+                        })}
+                    </div>
+
+                    <div id="back-cards" className="flex gap-x-8 overflow-x-auto columns-4 py-6 px-8 hidden
+                    xl:px-2 xl:flex xl:gap-x-4
+                    2xl:px-4 2xl:gap-x-8">
                     {backCards.map((card) => {
                         return (
                             <ArticleCard
-                            key={card.cardTitle}
-                            imageSource={card.imageSource}
-                            imageAlt={card.imageAlt}
-                            cardTitle={card.cardTitle}
-                            cardContent={card.cardContent}
-                            cardLink={card.cardLink}
-                        />
+                                key={card.cardTitle}
+                                imageSource={card.imageSource}
+                                imageAlt={card.imageAlt}
+                                cardTitle={card.cardTitle}
+                                cardContent={card.cardContent}
+                                cardLink={card.cardLink}
+                            />
                         )
                     })}
                 </div>
+                        
+                
             </section>
             <div className="h-8 absolute bottom-0 border-dashed border-r-2 border-white-light left-[50%] right-[50%]"></div>
         </div>
