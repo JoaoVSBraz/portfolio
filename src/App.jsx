85e07075aa56b854fec31a7cf5badb491510b1c1
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-02-27 08:11:15 -0300

    update back to top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,33 @@ import FirstScreen from "./components/FirstScreen/FirstScreen"
 import SecondScreen from "./components/SecondScreen/SecondScreen"
 import ThirdScreen from "./components/ThirdScreen/ThirdScreen"
 import Top from './assets/top.svg'
-import { useEffect, useState } from "react"
 
 export default function App() {
   const windowHeight = window.innerHeight
   const halfWindow = windowHeight / 2
-  const backToTopButton = document.querySelector('#to-top')
+  
+  window.addEventListener('scroll', handleScroll, false)
 
-  function watchWindow() {
+  function handleScroll() {
+    const backToTopButton = document.querySelector('#to-top')
     if (Math.abs(document.body.getBoundingClientRect().top) > halfWindow) {
       backToTopButton.classList.remove('hidden')
-    } else {
+    }else{
       backToTopButton.classList.add('hidden')
     }
   }
 
-  useEffect(() => {
-    function watchScroll() {
-      window.addEventListener('scroll', watchWindow)
-    }
-    watchScroll()
-    return () => { window.removeEventListener('scroll', watchWindow) }
-  }, [])
+  function handleClick(){
+    const header = document.querySelector('#header')
+    header.scrollIntoView({behavior: "smooth"})
+  }
 
   return (
-    <main className="relative bg-gray-dark font-saira font-medium text-center overflow-hidden md:text-lg">
+    <main className="relative bg-gray-dark font-saira font-medium text-center overflow-hidden md:text-lg" onScroll={handleScroll}>
       <FirstScreen />
       <SecondScreen />
       <ThirdScreen />
-      <img id="to-top" src={Top} alt="Botão voltar ao topo" className="hidden fixed w-12 z-50 bottom-4 right-4" />
+      <img onClick={handleClick} id="to-top" src={Top} alt="Botão voltar ao topo" className="hidden fixed w-12 z-50 bottom-4 right-4 hover:cursor-pointer" />
     </main>
   )
 }
